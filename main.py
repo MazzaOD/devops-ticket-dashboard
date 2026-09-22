7dfc364d9cb4fd484258e38f80d24786b87936ba
Mary O'Donnell <marybdizzle@gmail.com>
2026-09-22 13:30:54 +0100

    Second commit with functioning dashboard

--- main.py
+++ main.py
@@ -1,12 +1,49 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import psutil
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {
-        "message": "DevOps Ticket Dashboard is running"
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def dashboard(request: Request):
+
+    cpu = psutil.cpu_percent(interval=1)
+    memory = psutil.virtual_memory().percent
+    disk = psutil.disk_usage('/').percent
+
+    data = {
+        "cpu": cpu,
+        "memory": memory,
+        "disk": disk,
+        "open_tickets": 17,
+        "critical_tickets": 3,
+        "resolved_tickets": 42,
+        "events": [
+            "Ticket #231 Created",
+            "Deployment Successful",
+            "Container Restarted",
+            "Health Check Passed",
+            "Grafana Dashboard Updated"
+        ]
     }
+
+    print("data type:", type(data))
+    print("data:", data)
+
+    return templates.TemplateResponse(
+        request=request,
+        name="dashboard.html",
+        context={
+            **data
+        }
+    )
+
+
+
 
 @app.get("/health")
 def health():
